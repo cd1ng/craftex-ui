363bd92bab7c5a5f145bf047a5598cfaf4250466
cd1ng <cding0102@gmail.com>
2024-09-23 17:47:07 +0800

    feat: 🎸 添加基础打包配置

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import dts from 'vite-plugin-dts'
+
+function resolve(str: string) {
+  return path.resolve(__dirname, str);
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 从.ts(x)源文件中生成声明文件(*.d.ts)
+    dts()
+  ],
   resolve: {
     alias: {
       'craftex-ui': path.resolve(__dirname, './src/index.ts'),
@@ -14,5 +23,24 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+  },
+  build: {
+    outDir: "dist",
+    lib: {
+      entry: resolve("src/index.ts"),
+      name: "fish-ui-sy",
+      fileName: (format) => `craftex-ui.${format}.js`,
+      formats: ["cjs", "es", "umd"],
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+        format: "cjs",
+      },
+    },
   },
 })
